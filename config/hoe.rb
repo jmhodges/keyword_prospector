4cc83e5a6ab960f51ae7524ccb63331e62e51eb4
Alf <alf.mikula@latimes.com>
2008-08-11 14:32:06 -0700

    Adding hpricot dependency

--- config/hoe.rb
+++ config/hoe.rb
@@ -8,7 +8,7 @@ RUBYFORGE_PROJECT = 'keyword_prospector' # The unix name for your project
 HOMEPATH = "http://#{RUBYFORGE_PROJECT}.rubyforge.org"
 DOWNLOAD_PATH = "http://rubyforge.org/projects/#{RUBYFORGE_PROJECT}"
 EXTRA_DEPENDENCIES = [
-#  ['activesupport', '>= 1.3.1']
+  ['hpricot', '>= 0.6']
 ]    # An array of rubygem dependencies [name, version]
 
 @config_file = "~/.rubyforge/user-config.yml"
